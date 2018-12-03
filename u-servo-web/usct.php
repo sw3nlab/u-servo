943ab5246b438f698dd740e73d17721e30b79002
Kirishi Hackerspace <sw3n@mail.ru>
2018-12-03 12:44:44 +0300

    Update and rename index.php to usct.php

---
+++ u-servo-web/usct.php
@@ -0,0 +1,35 @@
+<?php
+
+
+echo "
+
+<html>
+<head>
+<title>ARM u-servo WEB interface</title>
+</head>
+
+<body>
+
+<center>
+
+<h1><tt>U-servo COMMAND TRAINER</tt></h1>
+<hr>
+<table border='0' width='90%' height='90%'>
+
+<tr>
+<td width='30%' align='center' valign='top'>COMMAND FRAME<br/><textarea name='cmd' cols='50' rows='30'></textarea></td>
+
+<td width='30%' align='center' valign='top'><br/>--------><br/><br/>РЎРћС…СЂР°РЅРёС‚СЊ СЌС‚РѕС‚ РЅР°Р±РѕСЂ РєРѕРјР°РЅРґ РєР°Рє:<br/><br/>--------><br/><br/>NAME:<input type='text' value='Р’Р·СЏС‚СЊ Р»РѕР¶РєСѓ =)' name='name'><br/><input type='submit' value='SAVE'></td>
+
+<td width='30%' align='center' valign='top'>COMMAND LIST</td>
+</tr>
+
+</table>
+</body>
+
+</html>
+
+"
+
+
+;?>
